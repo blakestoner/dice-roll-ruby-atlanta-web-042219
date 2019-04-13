357aed00bcae866d7db9dbfa06fab3dcf3cd6a04
Blake Stoner <stonerblake@gmail.com>
2019-04-13 13:48:18 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,5 +3,5 @@
 # Feel free to google "how to generate a random number in ruby"
 
 
-numbersArray = [1, 2, 3, 4, 5, 6]
-roll = numbersArray[rand(numbersArray.length)]
+Array = [1, 2, 3, 4, 5, 6]
+roll = Array[rand(Array.length)]
